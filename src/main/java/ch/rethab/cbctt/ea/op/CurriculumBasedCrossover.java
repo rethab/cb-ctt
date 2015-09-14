fabfb74d64da592fa4110cde4e0276c5265fdd81
Reto Hablützel <rethab@rethab.ch>
2015-09-14 19:18:04 +0200

    added curriclum based crossover

---
+++ src/main/java/ch/rethab/cbctt/ea/op/CurriculumBasedCrossover.java
@@ -0,0 +1,41 @@
+package ch.rethab.cbctt.ea.op;
+
+import ch.rethab.cbctt.domain.Specification;
+import ch.rethab.cbctt.ea.phenotype.MeetingWithRoom;
+import ch.rethab.cbctt.ea.phenotype.RoomAssigner;
+import ch.rethab.cbctt.ea.phenotype.TimetableWithRooms;
+import ch.rethab.cbctt.moea.SolutionConverter;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Inspired by 'New Crossover Algorithms for Timetabling with
+ * Evolutionary Algorithms' (Lewis and Paechter), this crossover
+ * operator takes one curriculum from the second parent and tries to
+ * replicate all lessons of all courses at the same periods in a copy
+ * of the first parent. That copy is the first child. The curriculum
+ * is selected at random. The second child is constructed by
+ * reversing the roles.
+ *
+ * @author Reto Habluetzel, 2015
+ */
+public final class CurriculumBasedCrossover extends AbstractLessonBasedCrossover {
+
+    public CurriculumBasedCrossover(SolutionConverter solutionConverter, RoomAssigner roomAssigner, Specification spec) {
+        super(solutionConverter, spec, roomAssigner);
+    }
+
+    @Override
+    protected Set<MeetingWithRoom> getMeetingsFromParent(TimetableWithRooms parent) {
+        String randomCurriculum = getRandomCurriculum();
+        return parent.getCurriculumTimetables().get(randomCurriculum).getAll().collect(Collectors.toSet());
+    }
+
+    private String getRandomCurriculum() {
+        int nCurricula = spec.getCurricula().size();
+        int idx = rand.nextInt(nCurricula);
+        return spec.getCurricula().get(idx).getId();
+    }
+
+}
