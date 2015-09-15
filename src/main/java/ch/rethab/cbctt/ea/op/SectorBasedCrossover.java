24d9d1e70eb36bd4ee8fcd310369ced411d6016c
Reto Hablützel <rethab@rethab.ch>
2015-09-15 20:53:52 +0200

    added sector based crossover

---
+++ src/main/java/ch/rethab/cbctt/ea/op/SectorBasedCrossover.java
@@ -0,0 +1,65 @@
+package ch.rethab.cbctt.ea.op;
+
+import ch.rethab.cbctt.domain.Specification;
+import ch.rethab.cbctt.ea.phenotype.MeetingWithRoom;
+import ch.rethab.cbctt.ea.phenotype.RoomAssigner;
+import ch.rethab.cbctt.ea.phenotype.TimetableWithRooms;
+import ch.rethab.cbctt.moea.SolutionConverter;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Inspired by 'New Crossover Algorithms for Timetabling with
+ * Evolutionary Algorithms' (Lewis and Paechter), this crossover
+ * operator takes a sector from the second parent and tries to
+ * replicate all lessons at the same periods in a copy
+ * of the first parent. That copy is the first child. The start
+ * of the sector is selected at random, while the size may
+ * be parametrized. The sector does not necessarily need to
+ * be within one day but may start in the evening of the
+ * first day and end in at noon on the second. A sector
+ * is chosen from one curriculum, which is also selected
+ * randomly. The second child is constructed by reversing
+ * the roles.
+ *
+ * @author Reto Habluetzel, 2015
+ */
+public final class SectorBasedCrossover extends AbstractLessonBasedCrossover {
+
+    /** number of lessons in one sector */
+    private final int sectorSize;
+
+    public SectorBasedCrossover(SolutionConverter solutionConverter, RoomAssigner roomAssigner, Specification spec, int sectorSize) {
+        super(solutionConverter, spec, roomAssigner);
+        this.sectorSize = sectorSize;
+    }
+
+    @Override
+    protected Set<MeetingWithRoom> getMeetingsFromParent(TimetableWithRooms parent) {
+        List<MeetingWithRoom> meetings = getSortedMeetings(parent);
+        int maxStartIdx = meetings.size() - sectorSize;
+        int startIdx = maxStartIdx <= 0 ? 0 : rand.nextInt(maxStartIdx);
+        int endIdx = startIdx + sectorSize;
+        if (endIdx > meetings.size()) {
+            endIdx = meetings.size();
+        }
+        return meetings.subList(startIdx, endIdx).stream().collect(Collectors.toSet());
+    }
+
+    private List<MeetingWithRoom> getSortedMeetings(TimetableWithRooms parent) {
+        int nCurricula = spec.getCurricula().size();
+        int idx = rand.nextInt(nCurricula);
+        String currId = spec.getCurricula().get(idx).getId();
+        return parent.getCurriculumTimetables().get(currId).getAll()
+                .sorted(Comparator.comparing(mwr -> toSlotIdx(mwr.getDay(), mwr.getPeriod())))
+                .collect(Collectors.toList());
+    }
+
+    private int toSlotIdx(int day, int period) {
+        return spec.getPeriodsPerDay() * day + period;
+    }
+
+}
---
+++ src/test/java/ch/rethab/cbctt/ea/op/SectorBasedCrossoverTest.java
@@ -0,0 +1,138 @@
+package ch.rethab.cbctt.ea.op;
+
+import ch.rethab.cbctt.domain.*;
+import ch.rethab.cbctt.ea.phenotype.GreedyRoomAssigner;
+import ch.rethab.cbctt.ea.phenotype.RoomAssigner;
+import ch.rethab.cbctt.ea.phenotype.TimetableWithRooms;
+import ch.rethab.cbctt.formulation.UD1Formulation;
+import ch.rethab.cbctt.moea.SolutionConverter;
+import org.junit.Test;
+import org.moeaframework.core.Solution;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Reto Habluetzel, 2015
+ */
+public class SectorBasedCrossoverTest {
+
+    int days = 3;
+    int periodsPerDay = 3;
+
+    Curriculum curr1 = new Curriculum("curr1");
+    Curriculum curr2 = new Curriculum("curr2");
+    Curriculum curr3 = new Curriculum("curr3");
+
+    Course c1 = Course.Builder.id("c1").teacher("t1").curriculum(curr1).nlectures(2)
+            .nWorkingDays(3).nStudents(3).doubleLectures(false).build();
+    Course c2 = Course.Builder.id("c2").teacher("t2").curriculum(curr2).nlectures(1)
+            .nWorkingDays(3).nStudents(3).doubleLectures(false).build();
+    Course c3 = Course.Builder.id("c3").teacher("t3").curriculum(curr3).nlectures(1)
+            .nWorkingDays(3).nStudents(3).doubleLectures(false).build();
+    Course c4 = Course.Builder.id("c4").teacher("t4").curriculum(curr1).curriculum(curr2)
+            .nlectures(1).nWorkingDays(3).nStudents(3).doubleLectures(false).build();
+
+    Room r1 = new Room("r1", 3, 0);
+    Room r2 = new Room("r2", 4, 1);
+
+    UnavailabilityConstraints unavailabilityConstraints = new UnavailabilityConstraints(days, periodsPerDay);
+    RoomConstraints roomConstraints = new RoomConstraints();
+
+    Specification spec = Specification.Builder.name("specification")
+            .days(days).periodsPerDay(periodsPerDay).minLectures(3).maxLectures(5)
+            .room(r1).room(r2)
+            .course(c1).course(c2).course(c3).course(c4)
+            .curriculum(curr1).curriculum(curr2).curriculum(curr3)
+            .unavailabilityConstraints(unavailabilityConstraints)
+            .roomConstraints(roomConstraints)
+            .build();
+
+    SolutionConverter solutionConverter = new SolutionConverter(new UD1Formulation(spec));
+
+    RoomAssigner roomAssigner = new GreedyRoomAssigner(spec);
+
+    @Test
+    public void shouldMoveSpecifiedNumberOfLessons() {
+        int sectorSize = 2; // two means the entire curriculum is moved
+        SectorBasedCrossover sectorBasedCrossover = new SectorBasedCrossover(solutionConverter, roomAssigner, spec, sectorSize);
+
+        Solution parent1 = solutionConverter.toSolution(TimetableWithRooms.Builder.newBuilder(spec)
+                .addMeeting(c1, r1, 0, 0).addMeeting(c1, r2, 0, 1)
+                .addMeeting(c2, r1, 1, 1).addMeeting(c2, r1, 1, 2)
+                .addMeeting(c3, r1, 0, 0).addMeeting(c3, r2, 0, 1)
+                .build()
+        );
+
+        Solution parent2 = solutionConverter.toSolution(TimetableWithRooms.Builder.newBuilder(spec)
+                .addMeeting(c1, r1, 1, 2).addMeeting(c1, r2, 2, 2)
+                .addMeeting(c2, r1, 1, 1).addMeeting(c2, r1, 2, 2)
+                .addMeeting(c3, r1, 2, 0).addMeeting(c3, r2, 2, 1)
+                .build()
+        );
+
+        Solution[] kids = sectorBasedCrossover.evolve(new Solution[]{parent1, parent2});
+        TimetableWithRooms kid1 = solutionConverter.fromSolution(kids[0]);
+
+        boolean curr1Moved = kid1.getMeeting(c1, 1, 2) != null && kid1.getMeeting(c1, 2, 2) != null;
+        boolean curr2Moved = kid1.getMeeting(c2, 1, 1) != null && kid1.getMeeting(c2, 2, 2) != null;
+        boolean curr3Moved = kid1.getMeeting(c3, 2, 0) != null && kid1.getMeeting(c3, 2, 1) != null;
+        // one should have moved..
+        assertTrue(curr1Moved || curr2Moved || curr3Moved);
+        // ..but not both
+        assertFalse(curr1Moved && curr2Moved && curr3Moved);
+        assertEquals(6, kid1.getMeetings().size());
+    }
+
+    @Test
+    public void shouldNotScrewUpWithCourseInTwoCurricula() {
+        int sectorSize = 2; // two means the entire curriculum is moved
+        SectorBasedCrossover sectorBasedCrossover = new SectorBasedCrossover(solutionConverter, roomAssigner, spec, sectorSize);
+
+        Solution parent1 = solutionConverter.toSolution(TimetableWithRooms.Builder.newBuilder(spec)
+                .addMeeting(c1, r1, 0, 0)
+                .addMeeting(c2, r1, 0, 0).addMeeting(c2, r1, 0, 1)
+                .addMeeting(c3, r1, 0, 1).addMeeting(c3, r1, 0, 2)
+                .addMeeting(c4, r1, 1, 0).addMeeting(c4, r2, 1, 1)
+                .build()
+        );
+
+        Solution parent2 = solutionConverter.toSolution(TimetableWithRooms.Builder.newBuilder(spec)
+                .addMeeting(c1, r1, 1, 0)
+                .addMeeting(c2, r1, 1, 0).addMeeting(c2, r1, 1, 1)
+                .addMeeting(c3, r1, 1, 1).addMeeting(c3, r1, 1, 2)
+                .addMeeting(c4, r1, 1, 2).addMeeting(c4, r2, 2, 1)
+                .build()
+        );
+
+        for (int i = 0; i < 20; i++) {
+            Solution[] kids = sectorBasedCrossover.evolve(new Solution[]{parent1, parent2});
+            TimetableWithRooms kid1 = solutionConverter.fromSolution(kids[0]);
+            TimetableWithRooms kid2 = solutionConverter.fromSolution(kids[1]);
+            assertEquals(7, kid1.getMeetings().size());
+            assertEquals(7, kid2.getMeetings().size());
+        }
+    }
+
+    @Test
+    public void shouldNotExplodeOnBounds() {
+        Solution parent1 = solutionConverter.toSolution(TimetableWithRooms.Builder.newBuilder(spec)
+            .addMeeting(c1, r1, 0, 0).addMeeting(c1, r2, 0, 1)
+            .addMeeting(c2, r1, 1, 1).addMeeting(c2, r1, 1, 2)
+            .addMeeting(c3, r1, 0, 0).addMeeting(c3, r2, 0, 1)
+            .build()
+        );
+
+        Solution parent2 = solutionConverter.toSolution(TimetableWithRooms.Builder.newBuilder(spec)
+            .addMeeting(c1, r1, 1, 2).addMeeting(c1, r2, 2, 2)
+            .addMeeting(c2, r1, 1, 1).addMeeting(c2, r1, 2, 2)
+            .addMeeting(c3, r1, 2, 0).addMeeting(c3, r2, 2, 1)
+            .build()
+        );
+
+        for (int sectorSize = 0; sectorSize < 10; sectorSize++) {
+            new SectorBasedCrossover(solutionConverter, roomAssigner, spec, sectorSize)
+                    .evolve(new Solution[]{parent1, parent2});
+        }
+    }
+
+}
